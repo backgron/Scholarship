2b0531d8f93d60b799099d1fe1ef52605af4aa6b
backgron <1319536124@qq.com>
2022-04-19 19:27:47 +0800

    完成申请展示页面

--- web/scholarship_control/src/pages/show/apply/components/allApply/allApply.jsx
+++ web/scholarship_control/src/pages/show/apply/components/allApply/allApply.jsx
@@ -7,51 +7,85 @@ import {
   InteractionFilled,
 } from "@ant-design/icons"
 import { Dialog, Toast, ProgressCircle } from "antd-mobile"
+import { useEffect, useState } from "react"
+import { findAllApply } from "../../../../../common/fetch"
+import { message } from "antd"
 
-const ShowBox = () => {
-  const mainColor = "#3d7bc7"
+const ShowBox = ({ item }) => {
+  let mainColor = "#3d7bc7"
   // const mainColor = "#ccc"
+  const isClassApply = item?.className ? true : false
+  const status = isClassApply
+    ? item.classStatus.status
+    : item.applyStatus.status
+  const statusConfig = (() => {
+    if (status === -1) {
+      mainColor = "#999"
+      return "已驳回"
+    } else if (status === 0) {
+      mainColor = "#ef7292"
+      return "学生已申请"
+    } else if (status === 1) {
+      mainColor = "#3d7bc7"
+      return "导员已审批"
+    } else if (status === 2) {
+      mainColor = "#3cb371"
+      return "申请已通过"
+    }
+  })()
 
   return (
     <div className="showBox">
-      <div className="mainBox">
+      <div className="mainBox" style={{ border: `1px solid ${mainColor}` }}>
         <div className="top">
           <div className="topLeft">
-            <h3>成绩修改</h3>
+            <h3>
+              {isClassApply ? item.className + "-成绩修改" : item.applyName}
+            </h3>
             <div className="topItem">
               <BookFilled style={{ color: mainColor }} />
-              <span>语文</span>
+              <span>
+                {isClassApply
+                  ? item.classStatus.newGrade + "分"
+                  : item.applyType}
+              </span>
             </div>
             <div className="topItem">
               <ClockCircleFilled style={{ color: mainColor }} />
-              <span>2022-04-17</span>
+              <span>
+                {isClassApply ? item.classStatus.applyTime : item.applyTime}
+              </span>
             </div>
             <div className="topItem">
               <InteractionFilled style={{ color: mainColor }} />
-              <span>辅导员已审批</span>
+              <span>{statusConfig}</span>
             </div>
           </div>
           <div className="topRight">
             <ProgressCircle
-              percent={30}
+              percent={30 * (status + 1)}
               style={{
                 "--fill-color": mainColor,
                 "--size": "90px",
                 "--track-width": "10px",
               }}
             >
-              <span>
-                30
-                <br />
-                次/天
-              </span>
+              <span>{statusConfig}</span>
             </ProgressCircle>
           </div>
         </div>
-        <div className="bottom" style={{ backgroundColor: mainColor }}>
+        <div
+          className="bottom"
+          style={{
+            background: mainColor,
+          }}
+        >
           <div
             className="bottomItem"
             onClick={() => {
+              if (status === 2) {
+                return message.error("已申请成功，无法撤销")
+              }
               Dialog.confirm({
                 content: "确定撤回申请？（撤回后不可恢复）",
                 onConfirm: async () => {
@@ -74,12 +108,25 @@ const ShowBox = () => {
 }
 
 export default () => {
+  const [data, setData] = useState(undefined)
+
+  useEffect(() => {
+    findAllApply({
+      success: (res) => {
+        let sortData = res.data.sort((a, b) => {
+          let timeA = a.className ? a.classStatus.applyTime : a.applyTime
+          let timeB = b.className ? b.classStatus.applyTime : b.applyTime
+          console.log(timeA, timeB)
+          return timeA > timeB ? 1 : -1
+        })
+        console.log(sortData)
+        setData(sortData)
+      },
+    })
+  }, [])
   return (
     <div className="allApply">
-      <ShowBox />
-      <ShowBox />
-      <ShowBox />
-      <ShowBox />
+      {data ? data.map((item) => <ShowBox item={item} key={item._id} />) : null}
     </div>
   )
 }
